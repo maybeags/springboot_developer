69f2033d93bf5308555d1b6215d05dc32f8b0b6f
maybeags <suren0709@gmail.com>
2024-12-27 18:23:43 +0900

    feat: chapter07 completed! CRUD and thymeleaf

--- chapter07/src/main/java/me/ahngeunsu/springbootdeveloper/controller/BlogViewController.java
+++ chapter07/src/main/java/me/ahngeunsu/springbootdeveloper/controller/BlogViewController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -49,5 +50,27 @@ public class BlogViewController {
         HTML 뷰 만들기
             01 단계 - resources/templates 디렉토리에 article.html을 만들어 화면을 작성합니다.
      */
-}
 
+    @GetMapping("/new-article")
+    // 1. id 키를 가진 쿼리 파라미터의 값을 id 변수에 매핑(id는 없을 수도 있음)
+    public String newArticle(@RequestParam(required = false) Long id, Model model) {
+        if (id == null) {   // 2. id가 null이라면 생성
+            model.addAttribute("article", new ArticleViewResponse());
+        } else {        // 3. id가 있으면 수정
+            Article article = blogService.findById(id);
+            model.addAttribute("article", new ArticleViewResponse(article));
+        }
+        return "newArticle";
+    }
+}
+/*
+    1. 쿼리 파라미터로 넘어온 id값은 newArticle() 메서드의 Long 타입 id 인자에 매핑합니다. 이 값은 없을 수도 있으므로
+    2. id가 있으면 수정, 없으면 생성합니다.
+        id가 없는 경우 기본 생성자를 이용해 빈 ArticleViewResponse 객체를 만들고, id가 있으면 기존 값을 가져오는 findById()
+        메서드를 호출합니다.
+
+    수정 / 생성 뷰 만들기
+
+        01 단계 - 이제 컨트롤러 메서드에서 반환하는 newArticle.html을 구현하겠습니다.
+            resources/templates 디렉토리에 newArticle.html 파일을 생성합니다.
+ */
--- chapter07/src/main/resources/static/js/article.js
+++ chapter07/src/main/resources/static/js/article.js
@@ -21,3 +21,66 @@ if(deleteButton) {
 
 //      02 단계 - [삭제] 버튼을 눌렀을 때 삭제하도록 [삭제] 버튼, 즉 button 엘리먼트에 delete-btn이라는 아이디 값을
 //          추가하고 앞서 작성한 article.js가 이 화면에서 동작하도록 import합니다.
+
+// 수정 기능
+// 1. id가 modify-btn인 엘리먼트 조회
+const modifyButton = document.getElementById('modify-btn');
+
+if (modifyButton) {
+    // 2. 클릭 이벤트가 감지되면 수정 API 요청
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
+        })
+        .then(() => {
+            alert('수정이 완료되었습니다.');
+            location.replace(`/articles/${id}`);
+        });
+    });
+}
+
+//이 코드는 id가 modify-btn인 엘리먼트를 찾고 그 엘리먼트에서 클릭 이벤트가 발생하면 id가 title, content인 엘리먼트 값을 가져와
+//fetch() 메서드를 통해 수정 API로 /api/articles로 PUT 요청을 보냅니다. 요청을 보낼 때는 headers에 요청 형식을 지정하고,
+//body에 HTML에 입력한 데이터를 JSON 형식으로 바꿔 보냅니다. 요청이 완료되면 then() 메서드로 마무리 작업을 합니다.
+//
+//03 단계 - 이제 article.html 파일을 열어 [수정] 버튼에 id값과 클릭 이벤트를 추가합니다. 기존에 입력했던 수정 버튼을 수정하면 됩니다.
+//      article.html 파일로 이동합니다.
+
+// 등록 기능
+// 1. id가 created-btn인 엘리먼트
+const createButton = document.getElementById("create-btn");
+
+if (createButton) {
+    // 2. 클릭 이벤트가 감지되면 생성 API 요청
+    createButton.addEventListener("click", (event) => {
+        fetch("/api/articles", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById("title").value,
+                content: document.getElementById("content").value,
+            }),
+        }).then(() => {
+            alert("등록 완료되었습니다.");
+            location.replace("/articles");
+        });
+    });
+}
+//1. id가 create-btn인 엘리먼트를 찾아 그 엘리먼트에서 2. 클릭 이벤트가 발생하면 id가 title, content인 엘리먼트의 값을 가져와
+//fetch() 메서드를 통해 생성 API로 /api/articles/ POST 요청을 보내줍니다.
+//
+//    02 단계 - 계속해서 articleList.html 파일을 수정해 id가 create-btn인 [생성] 버튼을 추가하겠습니다.
+//
+//        articleList.html 파일로 이동하세요.
