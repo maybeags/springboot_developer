6dca2281b6258d6cdc7aab3619a25e8a7fee67e4
maybeags <suren0709@gmail.com>
2024-12-30 17:44:33 +0900

    feat: chapter08 complete

--- chpater08/src/main/java/me/ahngeunsu/springbootdeveloper/service/UserDetailService.java
+++ chpater08/src/main/java/me/ahngeunsu/springbootdeveloper/service/UserDetailService.java
@@ -23,4 +23,11 @@ public class UserDetailService implements UserDetailsService {
 /*
     스프링 시큐리티에서 사용자 정보를 가져오는 UserDeailsService 인터페이스를 구현합니다(이름은 UserDetailService.java)
     필수로 구현해야 하는 loadUserByUsername() 메서드를 오버라이딩해서 사용자 정보를 가져오는 로직을 작성합님다.
+
+    3. 시큐리티 설정하기
+        01 단계 - 인증을 위한 도메인과 리포지토리, 서비스가 완성되었으니 실제 인증 처리를 하는 시큐리티 설정 파일
+            WebSecurityConfig.java 파일을 작성.
+            같은 패키지 내에 config 패키지를 새로 만들어 설정하면 됩니다.
+
+            springbootdeveloper에 우클릭 -> config 패키지 생성 -> WebSecurityConfig.java 생성
  */
